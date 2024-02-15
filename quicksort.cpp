a11efd30435504dec38e9e61c421b56a96999fb6
Moksh Gupta <115498284+mokshgupta03@users.noreply.github.com>
2024-02-15 20:53:34 +0530

    Create quicksort.cpp

---
+++ quicksort.cpp
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include <bits/stdc++.h>
+
+int arr[5];
+using namespace std;
+int p = 0;
+int r = 4;
+
+int partition(int* A,int p,int r)
+{
+    int x = A[r];
+    int i = p-1;
+
+    for(int j = p;j<r;j++)
+    {
+        if(A[j]<=x)
+        {
+            i++;
+            swap(A[j],A[i]);
+        }
+    }
+    swap(A[i+1],A[r]);
+    return i+1;
+}
+
+void quicksort(int *A,int p, int r)
+{
+    int q;
+    if(p<r)
+    {
+        q = partition(A,p,r);
+        quicksort(A,p,q-1);
+        quicksort(A,q+1,r);
+    }
+}
+
+
+int main()
+{
+
+    printf("Enter the elements of array\n");
+    for(int i = 0;i<5;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    quicksort(arr,p,r);
+
+    for(int i = 0;i<5;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+
+return 0;
+}
